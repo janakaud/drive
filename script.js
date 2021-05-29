9d3876e46688d7818428b02b8b00db6c76aa5983
Janaka Bandara <janakaud@gmail.com>
2021-05-29 12:30:35 +0530

    Script load/save

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
 var SCOPE = "https://www.googleapis.com/auth/script.projects";
 
-var url = "https://script.googleapis.com/v1/projects?fields=scriptId";
+var V1 = "https://script.googleapis.com/v1";
+var BASE = `${V1}/projects`;
+var url = BASE + "?fields=scriptId";
 
 function create() {
-	ajax("POST", url, {
+	_alert("POST", url, {
 		title: title.value,
 		parentId: parent.value
-	}, function(data) {
-		alert(JSON.stringify(data));
 	});
 }
 
+function _id() {
+	return id.value.match(/[^/?]{20,}/);
+}
+
+function load() {
+	ajax("GET", BASE + "/" + _id() + "/content", null, function(data) {
+		code.value = JSON.stringify(data);
+	});
+}
+
+function save() {
+	var data = JSON.parse(code.value);
+	_alert("PUT", `${BASE}/${_id()}/content?fields=scriptId`, {files: data.files.map(f => {
+		var {name, type, source, functionSet} = f;
+		return {name, type, source, functionSet};
+	})});
+}
+
 function search() {
-	ajax("GET", url + "&title=" + (title.value || ""), null, function(data) {
+	_get(`${url}&title=${title.value || ""}`);
+}
+
+function listDeployments() {
+	_get(`${BASE}/${_id()}/deployments`);
+}
+
+function listProcesses() {
+	_get(`${V1}/processes:listScriptProcesses?scriptId=${_id()}`);
+}
+
+function _get(_url) {
+	_alert("GET", _url, null);
+}
+
+function _alert(method, _url, _data) {
+	ajax(method, _url, _data, function(data) {
 		alert(JSON.stringify(data));
 	});
 }
